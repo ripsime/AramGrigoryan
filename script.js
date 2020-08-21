11d4d21314ae691ca43885e59039a6b5f6306cc6
MCUPrograms <mcuprograms@mail.ru>
2020-08-21 22:17:20 +0400

    working_version_1

---
+++ script.js
@@ -0,0 +1,7 @@
+let login_label = document.getElementById("flogin-label");
+let login_input = document.getElementById("flogin");
+
+login_input.onclick = function (){
+    login_label.innerText = "Login"
+
+}
